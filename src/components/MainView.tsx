3beb1f8d9b2701b415e8cf06b97af41a1cc04eb7
Fatpandac <i@fatpandac.com>
2023-06-13 20:58:56 +0800

    feat: Mainview support Action of reedit and regenerate

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -1,7 +1,7 @@
-import { useContext, useState } from "react";
-import { IndexContext } from "../context";
 import { Action, ActionPanel, Icon, List } from "@raycast/api";
 import dayjs from "dayjs";
+import { useContext, useState } from "react";
+import { IndexContext } from "../context";
 import { ShowDtail } from "./ShowDetail";
 
 export function MainView(props: { toggleMainView: () => void }) {
@@ -9,7 +9,7 @@ export function MainView(props: { toggleMainView: () => void }) {
   const { histories, handleSetArchives, handleSetHistories, clearHistories } = useContext(IndexContext);
 
   return (
-    <List onSearchTextChange={(text) => setInput(text)} searchBarPlaceholder="Input prompt">
+    <List searchText={input} onSearchTextChange={(text) => setInput(text)} searchBarPlaceholder="Input prompt">
       {histories.map((history) => (
         <List.Item
           title={history.prompt || "Ask me anything..."}
@@ -32,8 +32,25 @@ export function MainView(props: { toggleMainView: () => void }) {
                   />
                 }
               ></Action.Push>
+              <Action.Push
+                title="Regenerate"
+                icon={Icon.RotateClockwise}
+                onPush={() => {
+                  // clear input
+                  setInput("");
+                }}
+                target={
+                  <ShowDtail
+                    histories={histories}
+                    handleSetHistories={handleSetHistories}
+                    prompt={input || history.prompt}
+                    date={dayjs().valueOf()}
+                  />
+                }
+              ></Action.Push>
               <Action
                 title="Create New Chat"
+                shortcut={{modifiers: ["ctrl"], key: "enter"}}
                 icon={Icon.Message}
                 onAction={() => {
                   if (!histories[0].prompt) return;
@@ -46,6 +63,14 @@ export function MainView(props: { toggleMainView: () => void }) {
                 shortcut={{ modifiers: ["ctrl"], key: "c" }}
                 content={history.content}
               ></Action.CopyToClipboard>
+              <Action
+                title="Reedit Prompt"
+                shortcut={{ modifiers: ["cmd"], key: "r" }}
+                icon={Icon.TextCursor}
+                onAction={() => {
+                  setInput(history.prompt);
+                }}
+              ></Action>
               <Action
                 title="Toggle Open Archives"
                 icon={Icon.Tray}
